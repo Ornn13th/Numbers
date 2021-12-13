80f219ae743796a8cb5c0923e5a7becbef3651b7
Ornn13th <kovacsdaniel2015@gmail.com>
2021-12-13 15:09:56 +0100

    kész

--- 5.cs
+++ 5.cs
@@ -4,37 +4,31 @@ namespace age
 {
 
 
-    class age 
+    class age
     {
         static void Main(string[] args)
-        {     
+        {
 
             Console.Write("Kérem adja meg a nevét: ");
-            string name=Console.ReadLine();
+            string name = Console.ReadLine();
             Console.Write("Kérem adja meg születési évét : ");
             int birthdate = Convert.ToInt32(Console.ReadLine());
             string date = DateTime.Now.ToString("yyyy");
-            int year = Convert.ToInt32(DateTime.Now);
-            int age =year-birthdate;
-
-        }
-    
-        internal class NewBaseType
-    {
-        private static void age(ref string name , int age)
-
-        {
-            int m = Console.WindowHeight;
-            int sz = Console.WindowWidth;
-            Console.SetCursorPosition(sz / 2, m / 2);
-            Console.BackgroundColor = ConsoleColor.Blue;
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine(name);
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(age);
+            int year = Convert.ToInt32(date);
+            int age = year - birthdate;
+            Console.ReadKey();
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.Clear();
+                int m = Console.WindowHeight;
+                int sz = Console.WindowWidth;
+                Console.SetCursorPosition(sz / 2, m / 2);
+                Console.OpenStandardOutput();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(name);
+                Console.SetCursorPosition(sz / 2, m / 2+1);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(age);
         }
     }
-    }
-}    
+}  
            
